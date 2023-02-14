f0d84c9e12db05666b610d2acd23775d739ea42c
KittehKun <huntzachary30@gmail.com>
2023-02-14 17:14:14 -0600

    Completed 3rd challenge. Commented for code comprehension.

--- Coding Challenges/src/Program.cs
+++ Coding Challenges/src/Program.cs
@@ -4,8 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("[" + ArrayOfMultiples.Multiples(2, 5) + "]"); //Challenge #1
-        Console.WriteLine("[" + FindLargest.Largest(new int[] { 3, 4, 71 }, new int[] { 50, 12, 1 }, new int[] { 19, 22, 100 }) + "]"); //Challenge #2
+        //Challenge #1
+        Console.WriteLine("[" + ArrayOfMultiples.Multiples(2, 5) + "]");
+
+        //Challenge #2
+        Console.WriteLine("[" + FindLargest.Largest(new int[] { 3, 4, 71 }, new int[] { 50, 12, 1 }, new int[] { 19, 22, 100 }) + "]");
+
+        //Challenge #3
+        Console.WriteLine(SmoothSentences.IsSmooth("She eats super righteously"));
     }
 
 }
---
+++ Coding Challenges/src/challenges/SmoothSentences.cs
@@ -0,0 +1,26 @@
+﻿namespace Coding_Challenges.src.challenges
+{
+    /// <summary>
+    /// Challenge sourced from https://edabit.com/challenge/SkY5Nw3rS7WvkQmFc
+    /// </summary>
+    internal class SmoothSentences
+    {
+        public static bool IsSmooth(string sentence)
+        {
+            string[] words = sentence.Split(' '); //Splits words by space delimiter
+
+            for (int i = 0; i < words.Length - 1; i++) //Subtracts one from the length to correctly compare the last and second to last elements
+            {
+                string currentWord = words[i]; //Sets current word to the current index from i
+                string nextWord = words[i + 1]; //Sets the next work to the current index from i + 1
+
+                if (currentWord.Substring(currentWord.Length - 1, 1).ToLower() != nextWord.Substring(0, 1).ToLower()) //Takes substring of last letter of the first word and compares with the substring of the next word's first letter. If they are the same then continue looping through the array otherwise return false.
+                {
+                    return false;
+                }
+
+            }
+            return true; //If loop goes through all the way, then the sentence passed in was a 'Smooth Sentence'.
+        }
+    }
+}
