fb099dd65e97f0dacab4f8fd4f0ef8a6ebc993fc
KittehKun <huntzachary30@gmail.com>
2023-02-14 14:08:35 -0600

    Add project files.

---
+++ Array of Multiples/src/Program.cs
@@ -0,0 +1,11 @@
+﻿using Coding_Challenges.src.challenges;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("[" + ArrayOfMultiples.Multiples(2, 5) + "]");
+    }
+
+
+}
---
+++ Array of Multiples/src/challenges/ArrayOfMultiples.cs
@@ -0,0 +1,26 @@
+﻿namespace Coding_Challenges.src.challenges
+{
+    internal class ArrayOfMultiples
+    {
+        /// <summary>
+        /// This method takes in two arguments and returns an array of the input's multiples.
+        /// </summary>
+        /// <param name="num">Number used for multiples calculation.</param>
+        /// <param name="length">The length of the array.</param>
+        /// <returns>An array of multiples based on the <c>num</c> variable.</returns>
+        public static string Multiples(int num, int length)
+        {
+            int[] multipleArray = new int[length];
+            multipleArray[0] = num;
+            for (int i = 0; i < multipleArray.Length; i++)
+            {
+                int result = multipleArray[i] * 2;
+                if (i + 1 != multipleArray.Length)
+                {
+                    multipleArray[i + 1] = result;
+                }
+            }
+            return String.Join(", ", multipleArray);
+        }
+    }
+}
